1a198006498fb30789186c9d188e63dfef00d41b
lawyerbrs <ercan.kaya.com.tr@gmail.com>
2023-01-30 22:06:09 +0300

    Initial Commit

---
+++ hackingWithSwift100dayto1.playground/Contents.swift
@@ -0,0 +1,102 @@
+// hacking with swift Day 1
+
+// 1.Variables
+
+import Cocoa
+
+var str  = "Hello, playground"
+str = "Goodbye"
+
+ 
+// ( Why does Swift have variables ? )
+
+var favoriShow = "Orange is the New Black"
+favoriShow = "The good Place"
+favoriShow = "Doctor Who"
+
+// ( Variables )
+
+var messega = "Good morning, Dave!"
+var operatingSystem = "macOS"
+
+
+// 2. Strings and integers
+
+var str2 = "Hello, playground"
+var age = 38
+var population = 8_000_000
+
+var meaningOfLife = 42
+
+
+
+// 3. Multi-line strings
+
+var quote = "Change the world by being yourself"
+
+var burns = """
+The best laid schemes
+O'mice and men
+Gang aft agley
+"""
+
+// 4.Doubles and Boolens
+
+var myInt = 1
+var myDouble = 1.0
+
+
+// 5.String interpolation
+
+var score = 85
+var str3 = "Your score was \(score)"
+var results = "The test result are here: \(str3)"
+
+
+// 6.Constants
+
+let taylor = "swift"
+
+// 7.Type annotations
+
+let str4 = "Hello, playground"
+let album: String = "Reputation"
+let year: Int = 1990
+let height: Double = 1.78
+let taylorRocks: Bool = true
+
+
+// 8. Simple types:Summary
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
